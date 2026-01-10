2a2ae305fb5b15c14e940d69e26caba067011ba7
Tarun Kaushal <tarunkaushal123@gmail.com>
2026-01-10 19:56:39 +0530

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,4 @@
+First_Name = str(input("Enter your First Name: "))
+Last_Name = str(input("Enter your Last Name: "))
+
+print("Hello, " + First_Name + " " + Last_Name +"! Welcome to the Python Program")
